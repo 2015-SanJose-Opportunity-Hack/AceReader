96e918453e424767d8742976a07284f8ec285c4d
nkukday <neerajakukday@gmail.com>
2015-10-04 05:00:00 -0700

    startActivity

---
+++ AceReaderApp/app/src/main/java/appfactory/app/com/acereaderapp/StartQuizActivity.java
@@ -0,0 +1,18 @@
+package appfactory.app.com.acereaderapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by neerajakukday on 10/4/15.
+ */
+public class StartQuizActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startquiz_layout);
+
+
+
+    }
+}
