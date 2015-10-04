6a6ea8821a9f52ba28b207f92360482997fc0f7d
nkukday <neerajakukday@gmail.com>
2015-10-04 05:19:41 -0700

    startActivityComplete

--- AceReaderApp/app/src/main/java/appfactory/app/com/acereaderapp/StartQuizActivity.java
+++ AceReaderApp/app/src/main/java/appfactory/app/com/acereaderapp/StartQuizActivity.java
@@ -1,16 +1,36 @@
 package appfactory.app.com.acereaderapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by neerajakukday on 10/4/15.
  */
 public class StartQuizActivity extends Activity {
+
+    private ImageButton startQuiz;
+    private Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startquiz_layout);
+
+        mContext = this;
+        startQuiz=(ImageButton)findViewById(R.id.startQuiz);
+
+        startQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, QuizFragmentActivity.class);
+                mContext.startActivity(intent);
+
+            }
+        });
 
 
 
