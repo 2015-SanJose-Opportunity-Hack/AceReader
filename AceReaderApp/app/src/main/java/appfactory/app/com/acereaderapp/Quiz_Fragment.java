46752e687ae9bd937c512b78933b1cc62097b6e7
Prasad Shirsath <prasadss.in@gmail.com>
2015-10-04 11:04:27 -0700

    working on report

--- AceReaderApp/app/src/main/java/appfactory/app/com/acereaderapp/Quiz_Fragment.java
+++ AceReaderApp/app/src/main/java/appfactory/app/com/acereaderapp/Quiz_Fragment.java
@@ -2,7 +2,6 @@ package appfactory.app.com.acereaderapp;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
